3ff1065f31b147d5ff9d5234b46b48314f320a58
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2021-10-09 15:55:04 +0800

    Add magisk boot & simple recovery.img unpacker

--- payload.sh
+++ payload.sh
@@ -1,3 +1,15 @@
 #!/system/bin/sh
 
-echo 'Hello from payload.sh!'
+echo 'Preparing...'
+exec 2>&1
+export PATH="$PATH:/system/bin"
+cd '/tmp'
+chmod 755 'magiskboot'
+
+echo 'Dumping recovery image...'
+dd 'if=/dev/block/by-name/recovery' 'of=boot.img' || exit 1
+
+echo 'Unpacking recovery image...'
+./magiskboot unpack 'boot.img' || exit 1
+
+echo 'Done!'
